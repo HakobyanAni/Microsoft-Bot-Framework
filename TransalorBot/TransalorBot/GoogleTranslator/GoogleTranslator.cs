7969963b967b6595a86826bfd0e7035c67208468
AniHakobyan <annie2016l@gmail.com>
2019-06-09 00:22:00 +0400

    initial commit

---
+++ TransalorBot/TransalorBot/GoogleTranslator/GoogleTranslator.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.Translation.V2;
+
+namespace TransalorBot.GoogleTranslator
+{
+    public class GoogleTranslator
+    {
+        public string TranslateText(string wordToTranslate, string targetLang, string sourceLang)
+        {
+            var credential = GoogleCredential.FromJson(System.IO.File.ReadAllText(@"D:\Annie\Programming\GoogleTranslator\GoogleTranslatorAPI\Google\GT_credentials.json"));
+            TranslationClient client = TranslationClient.Create(credential);
+            var response = client.TranslateText(
+                text: $"{wordToTranslate}",
+                targetLanguage: "arm",  // Armenian
+                sourceLanguage: "en");  // English
+            return response.TranslatedText;
+        }
+    }
+}
